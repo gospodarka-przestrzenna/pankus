e5803e1afe521850dfb3f64aedca63668690cc5f
wzelazo <wiktor.zelazo@pwr.edu.pl>
2018-09-12 14:30:47 +0200

    add intersphinx module

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon']
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
