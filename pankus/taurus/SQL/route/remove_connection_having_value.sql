8621bd686298916f24083595ea31182f56231993
Maciej Kamiński <kaminski.maciej@gmail.com>
2021-03-13 20:43:40 +0100

    New helper funcion for removing described connections

---
+++ pankus/taurus/SQL/route/remove_connection_having_value.sql
@@ -0,0 +1,19 @@
+--'insert_connection.sql' writes 'connection' table by selecting pairs of points' id from 'network_geometry' table and weight of the connection from 'network_parameters' table
+
+
+DELETE FROM connection WHERE
+EXISTS
+(SELECT
+    *
+FROM
+    point as start,
+    point as end,
+    network_properties
+WHERE
+    connection.start_id=start.id AND
+    connection.end_id=end.id AND
+    network_properties.start = start.point AND
+    network_properties.end = end.point AND
+    network_properties.name = :key AND
+    network_properties.value = :value    
+);
