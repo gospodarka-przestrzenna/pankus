60c732f7e28c940042e6a7b488376b6717b8fe44
Maciej Kamiński <kaminski.maciej@gmail.com>
2021-03-13 14:00:11 +0100

    module version change

--- pankus/__init__.py
+++ pankus/__init__.py
@@ -5,4 +5,4 @@ from pkg_resources import get_distribution, DistributionNotFound
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
-    __version__ = "ERROR"
+    __version__ = "4.0"
