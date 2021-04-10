52cfcb234aa0b9602eda7f95a87dee41cc63cba1
Maciej Kamiński <kaminski.maciej@gmail.com>
2021-04-10 08:53:22 +0200

    Add cahching object for speedup

--- pankus/taurus/utils.py
+++ pankus/taurus/utils.py
@@ -48,3 +48,43 @@ def init_kwargs_as_parameters(function):
     def wrapper(self,*args,**kwargs):
         return function(self,*args,**{**self.kwargs,**kwargs})
     return wrapper
+
+
+
+class RouteCache:
+    def __init__(self,taurus_object):
+        self.taurus_object=taurus_object
+
+
+        self.points=[(point,id,od_idi) for (point,id,od_idi) in self.taurus_object.do('path/select_point')]
+                            
+        self.max_point=max([p[1] for p in self.points])+1
+        self.max_od_point=max([p[2] for p in self.points if p[2]])+1
+        self.od_id=[None for i in range(self.max_point)]
+        
+        for p in self.points:
+            self.od_id[p[1]]=p[2]
+        
+        self.featured_points=[p[2] for p in self.points if p[2]!=None]
+
+    def cache_distances(self):
+        assert self.taurus_object.table_exists('distance')
+        self.distance=[[None for i in range(self.max_point)] for j in range(self.max_od_point)]
+
+        for start_id,end_id,predecessor_id,successor_id,weight in self.taurus_object.do('path/select_routes'):
+            self.distance[self.od_id[start_id]][end_id]=[predecessor_id,weight]
+
+    def cache_motion_exchange(self):
+        assert self.taurus_object.table_exists('motion_exchange')
+        self.motion_exchange=[[None for i in range(self.max_od_point)] for j in range(self.max_od_point)]
+        for start,end,value in self.taurus_object.do('stress/select_motion_exchange'):
+            self.motion_exchange[start][end]=value
+
+    def cache_stress(self):
+            assert self.taurus_object.table_exists('stress')
+            self.stress=[{} for i in range(self.max_point)]
+            for start_id,end_id,stress in self.taurus_object.do('stress/select_stress'):
+                self.stress[start_id][end_id]=stress
+
+
+
