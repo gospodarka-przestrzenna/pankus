9898f6b40e42c40d33e0c57e49856008bf4db22d
Maciej Kamiński <kaminski.maciej@gmail.com>
2025-01-04 14:57:24 +0100

    alternative limit destinations

--- pankus/taurus/SQL/intopp/eradicate_celling_destinations.sql
+++ pankus/taurus/SQL/intopp/eradicate_celling_destinations.sql
@@ -1,57 +1,110 @@
-WITH fcs(id,floor,celling) AS (
+CREATE TABLE temp_compute_values  (
+          id integer,
+          destinations real
+          );
+
+WITH fcs(id,_floor,celling) AS (
     SELECT
         od_id,
         (select value from od_properties
-        where od_geometry.od_id=od_id and name = :floor_name LIMIT 1),
+        where od_geometry.od_id=od_id and name = :floor_name LIMIT 1)+0 as _floor,
         (select value from od_properties
-        where od_geometry.od_id=od_id and name = :celling_name LIMIT 1)
+        where od_geometry.od_id=od_id and name = :celling_name LIMIT 1)+0 as celling
     FROM
         od_geometry
 ),
-sum_org_plan(s_org,s_planned) as (
-    SELECT 
-        SUM(
-            roof.destinations
-        ),
+sums(surplus, deficyt, max_surplus, max_deficyt) AS(
+    SELECT
         SUM(
             CASE 
-                WHEN model_parameters.destinations > fcs.celling then fcs.celling
-                WHEN model_parameters.destinations < fcs.floor   then fcs.floor            
-                ELSE model_parameters.destinations
+                WHEN model_parameters.destinations > fcs.celling then model_parameters.destinations-fcs.celling
+                ELSE 0
             END
-        )
+        ) as surplus,
+        SUM(
+            CASE
+                WHEN model_parameters.destinations < fcs._floor then fcs._floor-model_parameters.destinations
+                ELSE 0
+            END
+        ) as deficyt,
+        SUM(
+            CASE 
+                WHEN model_parameters.destinations > fcs._floor AND
+                        model_parameters.destinations < fcs.celling 
+                        THEN fcs.celling-model_parameters.destinations
+                ELSE 0
+            END
+        ) as maximal_surplus,
+        SUM(
+            CASE 
+                WHEN model_parameters.destinations > fcs._floor AND
+                        model_parameters.destinations < fcs.celling 
+                        THEN model_parameters.destinations-fcs._floor
+                ELSE 0
+            END
+        ) as maximal_deficyt
     FROM
         model_parameters,
-        roof,
         fcs
     WHERE 
-        fcs.id == model_parameters.od_id AND
-        roof.od_id == model_parameters.od_id
+        model_parameters.od_id == fcs.id
 ),
-new_destinations(od_id,destinations) AS (
+chg_fractions(bump_fraction,reduce_fraction) AS (
+    SELECT
+        CASE
+            WHEN sums.surplus > sums.deficyt AND (sums.surplus - sums.deficyt) > sums.max_surplus THEN
+                1/0
+            ELSE
+                CASE
+                    WHEN sums.surplus > sums.deficyt THEN
+                        (sums.surplus - sums.deficyt) / sums.max_surplus
+                    ELSE
+                        0
+                END
+        END as bump_fraction,
+        CASE
+            WHEN sums.deficyt > sums.surplus AND (sums.deficyt - sums.surplus) > sums.max_deficyt THEN
+                1/0 
+            ELSE 
+                CASE
+                    WHEN sums.deficyt > sums.surplus THEN
+                        (sums.deficyt - sums.surplus) / sums.max_deficyt
+                    ELSE
+                        0
+                END
+        END as reduce_fraction
+    FROM
+        sums
+),
+compute_values(od_id,destinations) AS (
     SELECT
         mp.od_id,
         CASE
-            WHEN mp.destinations < fcs.floor THEN fcs.floor + r.destinations * (sum_org_plan.s_org - sum_org_plan.s_planned) / sum_org_plan.s_org
-            WHEN mp.destinations > fcs.celling THEN fcs.celling + r.destinations * (sum_org_plan.s_org - sum_org_plan.s_planned) / sum_org_plan.s_org
-            ELSE mp.destinations + r.destinations * (sum_org_plan.s_org - sum_org_plan.s_planned) / sum_org_plan.s_org
+            WHEN mp.destinations < fcs._floor THEN fcs._floor
+            WHEN mp.destinations > fcs.celling THEN fcs.celling
+            ELSE
+                CASE
+                    WHEN chg_fractions.reduce_fraction > 0 THEN
+                        mp.destinations - (mp.destinations - fcs._floor) * ( chg_fractions.reduce_fraction)
+                    WHEN chg_fractions.bump_fraction > 0 THEN
+                        mp.destinations + (fcs.celling - mp.destinations) * ( chg_fractions.bump_fraction)
+                    ELSE
+                        mp.destinations
+                END
         END
     FROM
         model_parameters as mp,
-        roof as r,
         fcs,
-        sum_org_plan
+        chg_fractions
     WHERE
-        fcs.id == r.od_id AND
-        r.od_id == mp.od_id
+        fcs.id == mp.od_id
 )
+INSERT INTO temp_compute_values
+SELECT od_id, destinations 
+FROM compute_values;
+
 UPDATE model_parameters
-SET 
-    destinations = (
-    SELECT
-        destinations
-    FROM 
-        new_destinations
-    WHERE 
-        model_parameters.od_id == new_destinations.od_id
-    )
+SET destinations = (SELECT destinations FROM temp_compute_values WHERE temp_compute_values.id = model_parameters.od_id)
+WHERE EXISTS (SELECT * FROM temp_compute_values WHERE temp_compute_values.id = model_parameters.od_id);
+
+DROP TABLE temp_compute_values;
