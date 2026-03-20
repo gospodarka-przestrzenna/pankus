4b727c658d1bc1343615c06c4f1d7e8098f43392
Maciej Kamiński <kaminski.maciej@gmail.com>
2026-03-20 10:42:12 +0100

    Test change ow pkg_resource

--- pankus/__init__.py
+++ pankus/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __authors__ = 'Wawrzynec Zipser, Maciej Kamiński, Wiktor Żelazo - Politechnika Wrocławska'
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import version, PackageNotFoundError
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version("pankus")
+except PackageNotFoundError:
     __version__ = "4.0"
